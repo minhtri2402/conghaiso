1a71148925dfdc2a93057068f8d63ba008501ebd
minhtri2402 <43946752+minhtri2402@users.noreply.github.com>
2018-12-15 00:06:49 +0700

    Add files via upload

---
+++ Sumtwonumber.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.Scanner;
+public class Sumtwonumber {
+	public static void main(String[] args){
+		int a,b;
+		
+		Scanner sc = new Scanner(System.int);
+		
+		System.out.print("Enter number 1: ");
+		a = sc.newInt();
+		System.out.print("Enter number 2: ");
+		b = sc.newInt();
+		
+		int c=a+b;
+		System.out.println(a "+" b "=" c);
+	}
+
+
+}
